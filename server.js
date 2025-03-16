0b1ef2b9d183fdd271beb058b23c800ffa7847ad
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-16 21:18:33 +0530

    Use requests and responses

--- server.js
+++ server.js
@@ -2,6 +2,20 @@
 
 const express = require("express");
 const app = express();
+
+app.get("/" , function(req , res){
+    res.send("<h1>Hello World</h1>");
+});
+
+
+// app.get("/" , function(req,res){
+//     console.log(request);
+// })
+
+
+// app.get("/" , function(req,res){
+//     response.send("Hello");
+// })
 app.listen(3000 , function(){
     console.log("Server started on port 3000");
 });
