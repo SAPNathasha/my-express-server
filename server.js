5ae56e0a494e3caf870afdbebd402a78e728af91
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-16 21:53:21 +0530

    Use routes with nodemon

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.get("/" , function(req , res){
     res.send("<h1>Hello World</h1>");
 });
 
+app.get("/contact" , function(req , res){
+    res.send("<p>Contact me : 0772334571");
+});
+
+app.get("/about" , function(req,res){
+    res.send("I'm Prabhavi Nathasha. I am an IT undergraduate at university of Moratuwa. I love coding.")
+});
+
+app.get("/hobbies" , function(req, res){
+    res.send("<ul><li>Drawing pictures</li>, <li>Singing</li>, <li>Gardening</li>");
+});
 
 // app.get("/" , function(req,res){
 //     console.log(request);
